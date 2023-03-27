9c81b93410ac5ecfb5d2a030fa8cef34514e33b3
Agnik Bakshi <77234005+Agnik7@users.noreply.github.com>
2023-03-27 05:35:41 +0530

    FIrst commit

---
+++ Black_object/Black_object.ino
@@ -0,0 +1,27 @@
+//Code to detect black object using infrared sensor.
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (13,OUTPUT);
+  pinMode (10,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int SensorValue = digitalRead(10);
+  Serial.print("IR pin value: ");
+  Serial.print(SensorValue);
+  if(SensorValue == LOW)
+  {
+    digitalWrite(13,HIGH);
+    Serial.println("Object not black");
+    }
+  else
+  {
+    digitalWrite(13,LOW);
+    Serial.println("Object Black");
+    }
+    delay(100);
+  
+
+}
