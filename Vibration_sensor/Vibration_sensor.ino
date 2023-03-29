6d090b9b242aa6622456b94ce5433063399fc50e
Agnik Bakshi <77234005+Agnik7@users.noreply.github.com>
2023-03-29 16:14:14 +0530

    Create Vibration_sensor.ino

---
+++ Vibration_sensor/Vibration_sensor.ino
@@ -0,0 +1,36 @@
+// Code to detect vibration using vibration sensor
+int Dig_pin = 7;
+int Ana_pin = A0;
+int Dig_out = LOW;
+int Ana_out = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(Dig_pin, INPUT);
+  pinMode(Ana_pin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Dig_out = digitalRead(Dig_pin);
+  Ana_out = analogRead(Ana_pin);
+  Serial.print("Analog: ");
+  Serial.print(Ana_out);
+  Serial.print("  Digital: ");
+  Serial.print(Dig_out);
+  if(Ana_out <= 500){
+    digitalWrite(12,HIGH);
+    Serial.println("Vibration Detected!!!!!");
+    Serial.println();
+    Serial.println();
+    }
+  else
+  {
+    
+    digitalWrite(12,LOW);
+    Serial.println("Vibration NOT Detected");
+    Serial.println();
+    Serial.println();
+    }
+  delay(500);
+}
