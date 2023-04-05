f738867e2d5385fb6593f4c49cbff1a92cec3417
Agnik Bakshi <77234005+Agnik7@users.noreply.github.com>
2023-04-05 16:12:26 +0530

    Create Gas_detect.ino

---
+++ Gas_detect/Gas_detect.ino
@@ -0,0 +1,22 @@
+// Gas Detection using MQ-135 gas sensor
+// MQ-135 detects air quality (benzene, alcohol, smoke)
+
+
+float anaPin = A0;
+int digPin = 12;
+int dig_val;
+float ana_val;
+void setup(){
+  // put your setup code here, to run once:
+  pinMode(anaPin, INPUT);
+  pinMode(digPin, INPUT);
+  Serial.begin(9600);
+  Serial.print("Sensor is warming");
+  delay(20000);
+}
+
+void loop(){
+  // put your main code here, to run repeatedly:
+  ana_val = analogRead(anaPin);
+  dig_val = digitalRead(digPin);
+}
