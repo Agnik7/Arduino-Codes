01f387151638427205daf7a00ee4d8e33718f9e0
Alik Agarwala <alikagarwala@outlook.com>
2023-07-22 00:18:14 +0530

    Refactor code to improve readability and maintainability
    Changes Made:
    
    1. Constant definition for pin number
    2. Variable naming
    3. Variable increments

--- Brightness_alter/Brightness_alter.ino
+++ Brightness_alter/Brightness_alter.ino
@@ -1,21 +1,18 @@
-//Code to alter brightness of light
-int ledpin = 11, brightness = 0, fadeAmount= 5;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode (ledpin,OUTPUT);
-
+// Code to alter brightness of light
+#define LED_PIN 11
+int brightness = 0, fadeAmount = 5;
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(LED_PIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-  analogWrite(ledpin, brightness);
-  brightness = brightness + fadeAmount;
-  if(brightness <=0 || brightness >= 255)
-  {
-      fadeAmount = -fadeAmount;
-    }
-  delay(30);
-  
-
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    analogWrite(LED_PIN, brightness);
+    brightness += fadeAmount;
+    if (brightness <= 0 || brightness >= 255)
+        fadeAmount = -fadeAmount;
+    delay(30);
 }
