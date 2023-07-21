8cf6542c6b50380f5d2451da12440fccb63da4ca
Alik Agarwala <alikagarwala@outlook.com>
2023-07-22 00:08:19 +0530

    Refactor code to simplify if conditions and improve readability
    Properly indented the code to enhance readability and maintain a consistent spacing pattern throughout the code.
    
    Replaced SensorValue == LOW with !SensorValue to improve code clarity. Since digitalRead returns a boolean value (HIGH or LOW), we can directly use the ! operator to check if the infrared sensor detects an object.

--- Black_object/Black_object.ino
+++ Black_object/Black_object.ino
@@ -1,27 +1,27 @@
-//Code to detect black object using infrared sensor.
-void setup() {
-  // put your setup code here, to run once:
-  pinMode (13,OUTPUT);
-  pinMode (10,INPUT);
-  Serial.begin(9600);
+// Code to detect black object using infrared sensor.
+void setup()
+{
+    // put your setup code here, to run once:
+    pinMode(13, OUTPUT);
+    pinMode(10, INPUT);
+    Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int SensorValue = digitalRead(10);
-  Serial.print("IR pin value: ");
-  Serial.print(SensorValue);
-  if(SensorValue == LOW)
-  {
-    digitalWrite(13,HIGH);
-    Serial.println("Object not black");
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    int SensorValue = digitalRead(10);
+    Serial.print("IR pin value: ");
+    Serial.print(SensorValue);
+    if (!SensorValue)
+    {
+        digitalWrite(13, HIGH);
+        Serial.println("Object is not black");
     }
-  else
-  {
-    digitalWrite(13,LOW);
-    Serial.println("Object Black");
+    else
+    {
+        digitalWrite(13, LOW);
+        Serial.println("Object is Black");
     }
     delay(100);
-  
-
 }
